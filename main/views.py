68bf96c4f6ebe4cf9f2ea66f132646d762db2d1b
Aleksei Ivashin <alekseyivashin806@yandex.ru>
2017-01-10 00:10:48 +0300

    удаление записи через ajax

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .forms import RecordForm
 from .models import Record
 from datetime import datetime
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 
 
 def cut_text(records):
@@ -33,11 +33,16 @@ def record_new(request):
 
 
 def one_record(request, record_id):
-    record = get_object_or_404(Record, pk=record_id)
-    return render(request, "one_record.html", {'record': record})
+    if request.method == "GET":
+        record = get_object_or_404(Record, pk=record_id)
+        return render(request, "one_record.html", {'record': record})
+    elif request.method == "DELETE":
+        Record.objects.get(pk=record_id).delete()
+        return HttpResponse("ok")
+
 
 
 def user_records(request, user_id):
-    records = Record.objects.all().filter(author_id=user_id)
+    records = Record.objects.all().filter(author_id=user_id).order_by("-created_date")
     records = cut_text(records)
     return render(request, "user_records.html", {"records": records})
